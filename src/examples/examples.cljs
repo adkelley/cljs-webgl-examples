a098945e09992f96d5274b1f3c1c2fb5a87809d5
Alex Kelley <adkelley@users.noreply.github.com>
2016-12-14 10:40:02 -0800

    01-00-01 WIP

--- src/examples/examples.cljs
+++ src/examples/examples.cljs
@@ -11,66 +11,70 @@
 
 (defonce transform (r/atom {:tx -50 :ty 50 :tz -400
                             :rx 0 :ry 45 :rz 0
-                            :sx 1 :sy 1 :sz 1}))
+                            :sx 1.0 :sy 1.0 :sz 1.0}))
 
-(defn slider [param value min max step]
-  [:div {:class "slider"}
-    [:input {:type "range" :value value :min min :max max :step step
-             :on-change (fn [e]
-                          (swap! transform assoc param (.-target.value e))
-                          (let [{:keys [tx ty tz
-                                        sx sy sz
-                                        rx ry rz]} @transform]
-                             (texture/draw [tx ty tz] [rx ry rz] [sx sy sz])))}]])
+; (defn slider [param value min max step]
+;   [:div {:class "slider"}
+;     [:input {:type "range" :value value :min min :max max :step step
+;              :on-change (fn [e]
+;                           (swap! transform assoc param (.-target.value e))
+;                           (let [{:keys [tx ty tz
+;                                         sx sy sz
+;                                         rx ry rz]} @transform]
+;                              (texture/draw [tx ty tz] [rx ry rz] [sx sy sz])))}]])
 
+(defn draw-slider
+  [{:keys [label param attributes]}]
+  [:div
+    (str label ": ") (param @transform)
+    [:div {:class "slider"}
+      [:input (hash-map
+                :type "range"
+                :value (param @transform) :min (:min attributes)
+                :max (:max attributes) :step (:step attributes)
+                :on-change (fn [e]
+                             (swap! transform assoc param (.-target.value e))
+                             (let [{:keys [tx ty tz
+                                           sx sy sz
+                                           rx ry rz]} @transform]
+                               (texture/draw [tx ty tz] [rx ry rz] [sx sy sz]))))]]])
 
-(defn scale-panel []
-   (let [{:keys [sx sy sz]} @transform]
-     [:div {:class "panel"}
-       [:h3 "Scaling"]
-       [:div {:class "x"}
-        "X: " (float sx)
-        [slider :sx sx 1 5 0.25]]
-       [:div {:class "y"}
-        "Y: " (float sy)
-        [slider :sy sy 1 5 0.25]]
-       [:div {:class "z"}
-        "Z: " (float sz)
-        [slider :sz sz 1 5 0.25]]]))
+(defn draw-panel
+  [{:keys [label sliders]}]
+  [:div {:class "panel"}
+    [:h3 label]
+    (map draw-slider sliders)])
+;
 
-(defn translate-panel []
-  (let [{:keys [tx ty tz]} @transform]
-    [:div {:class "panel"}
-      [:h3 "Translation"]
-      [:div {:class "x"}
-       "X: " (int tx)
-       [slider :tx tx -200 200 1]]
-      [:div {:class "y"}
-       "Y: " (int ty)
-       [slider :ty ty -200 200 1]]
-      [:div {:class "z"}
-       "Z: " (int tz)
-       [slider :tz tz -1000 0 1]]]))
+(defonce translate-sliders
+  (let [attributes {:min -200 :max 200 :step 1}]
+    [{:label "X" :param :tx :attributes attributes}
+     {:label "Y" :param :ty :attributes attributes}
+     {:label "Z" :param :tz :attributes attributes}]))
 
-(defn rotate-panel []
-   (let [{:keys [rx ry rz]} @transform]
-     [:div {:class "panel"}
-       [:h3 "Rotation"]
-       [:div {:class "x"}
-        "X: " (int rx)
-        [slider :rx rx 0 359 1]]
-       [:div {:class "y"}
-        "Y: " (int ry)
-        [slider :ry ry 0 359 1]]
-       [:div {:class "z"}
-        "Z: " (int rz)
-        [slider :rz rz 0 359 1]]]))
+(defonce scale-sliders
+  (let [attributes {:min 1.0 :max 5.0 :step 0.25}]
+    [{:label "X" :param :sx :attributes attributes}
+     {:label "Y" :param :sy :attributes attributes}
+     {:label "Z" :param :sz :attributes attributes}]))
+
+(defonce rotate-sliders
+  (let [attributes {:min 0 :max 359 :step 1}]
+    [{:label "X" :param :rx :attributes attributes}
+     {:label "Y" :param :ry :attributes attributes}
+     {:label "Z" :param :rz :attributes attributes}]))
 
 (defn ui-panel []
   [:div {:id "ui-panel"}
-    [translate-panel]
-    [rotate-panel]
-    [scale-panel]])
+    [draw-panel :key 1
+      {:label "Translation"
+       :sliders translate-sliders}]
+    [draw-panel :key 2
+      {:label "Rotation"
+       :sliders rotate-sliders}]
+    [draw-panel :key 3
+      {:label "Scaling"
+       :sliders scale-sliders}]])
 
 (defn on-js-reload [])
 
